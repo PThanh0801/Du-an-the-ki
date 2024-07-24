1e6795ecbe0652ed17ba0d487f28b2d7d13b68c6
PThanh0801 <127266610+PThanh0801@users.noreply.github.com>
2024-07-24 17:30:08 +0700

    Update responses.py

--- Seminar/responses.py
+++ Seminar/responses.py
@@ -7,8 +7,4 @@ def sample_responses(input_text):
         return 'Im the bank bot'
     if user_msg in("ngu", "bot ngu"):
         return "co m ngu dcmm"
-    if user_msg in ("bulul", "them bulul","bulu",):
-        return "bú cmm à"
-    if user_msg in ("dcmm", "dcm", "fuck", "fuk", 'cặc', 'buồi', "dmm","lồn", "bướm",   ):
-        return "chủi thề con cặc"
     return 'dcmm'
